184fc616b3824fc78a3a1354964373ba9f3db07d
steadfast95 <shker1995@gmail.com>
2026-06-17 21:56:20 +0300

    fix question

--- src/main/java/ru/home/digexco/DigexcoGrpcClient.java
+++ src/main/java/ru/home/digexco/DigexcoGrpcClient.java
@@ -1,32 +1,43 @@
 package ru.home.digexco;
 
-import ch.qos.logback.core.testUtil.RandomUtil;
 import com.example.grpc.Digexco;
 import com.example.grpc.SequenceServiceGrpc;
 import io.grpc.stub.StreamObserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class DigexcoGrpcClient {
     private static final Logger logger = LogManager.getLogger(DigexcoGrpcClient.class);
 
+    private static final int maxIterableValue = 50;
+
+    private final AtomicInteger atomicInteger = new AtomicInteger(0);
+    private final long ONE_SECOND = 1000L;
     private final SequenceServiceGrpc.SequenceServiceStub async;
+    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
 
     public DigexcoGrpcClient(SequenceServiceGrpc.SequenceServiceStub async) {
         this.async = async;
     }
 
+    @Async
     void calling() {
-        int positiveInt = RandomUtil.getPositiveInt();
-        logger.info("Starting gRPC call with number: {}", positiveInt);
+        int p0 = 0;
+        int p1 = 30;
+        logger.info("Starting gRPC call with numbers: {} and {}", p0, p1);
 
         StreamObserver<Digexco.ServerResponse> responseObserver = new StreamObserver<Digexco.ServerResponse>() {
             @Override
             public void onNext(Digexco.ServerResponse response) {
+                atomicInteger.set(response.getNumber());
                 logger.info("Received number from server: {}", response.getNumber());
             }
 
@@ -41,11 +52,26 @@ public class DigexcoGrpcClient {
             }
         };
 
+        executorService.submit(()->{
+            try {
+                for (int i = 0; i < maxIterableValue; i++) {
+                    logger.info("some current value = {}", i + atomicInteger.getAndAdd(1));
+                    TimeUnit.MILLISECONDS.sleep(ONE_SECOND);
+                }
+            } catch (InterruptedException e) {
+                logger.error(e);
+                Thread.currentThread().interrupt();
+            } catch (Exception e) {
+                logger.error(e);
+            }
+        });
+
         async
                 .withDeadlineAfter(120, TimeUnit.SECONDS) // Таймаут 120 с
                 .getSeq(
                         Digexco.ClientRequest.newBuilder()
-                                .setNumber(positiveInt)
+                                .setNumber(p0)
+                                .setNumber2(p1)
                                 .build(),
                         responseObserver
                 );
--- src/main/java/ru/home/digexco/DigexcoGrpcServer.java
+++ src/main/java/ru/home/digexco/DigexcoGrpcServer.java
@@ -6,7 +6,10 @@ import io.grpc.stub.StreamObserver;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.grpc.server.service.GrpcService;
+import org.springframework.scheduling.annotation.Async;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @GrpcService
@@ -14,14 +17,18 @@ public class DigexcoGrpcServer extends SequenceServiceGrpc.SequenceServiceImplBa
     private static final Logger logger = LogManager.getLogger(DigexcoGrpcServer.class);
 
     private final int maxSequence = 50;
-    private final long ms = 2000L;
+    private final long TWO_SECOND = 2000L;
+
 
     @Override
+    @Async
     public void getSeq(Digexco.ClientRequest request, StreamObserver<Digexco.ServerResponse> responseObserver) {
         int number = request.getNumber();
+        int number2 = request.getNumber2();
         logger.info("get number by grpc {}", number);
         try {
-            for (int i = 1; i < maxSequence; i++) {
+
+            for (int i = 1; i < number2; i++) {
                 int newValue = number + i;
                 Digexco.ServerResponse response = Digexco.ServerResponse.newBuilder()
                         .setNumber(newValue)
@@ -29,9 +36,13 @@ public class DigexcoGrpcServer extends SequenceServiceGrpc.SequenceServiceImplBa
 
                 responseObserver.onNext(response);
                 logger.info("send to client new number {}", newValue);
-                TimeUnit.MILLISECONDS.sleep(ms);
+                TimeUnit.MILLISECONDS.sleep(TWO_SECOND);
             }
             responseObserver.onCompleted();
+        } catch (InterruptedException e) {
+            logger.error(e);
+            responseObserver.onError(e);
+            Thread.currentThread().interrupt();
         } catch (Exception e) {
             responseObserver.onError(e);
         }
